18a90c13747371775ee8a0e7d677d8d1dd4e3a02
MahtaFetrat <62302965+MahtaFetrat@users.noreply.github.com>
2020-04-06 12:35:55 +0430

    loggin wih tester

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("hi we make it :)))");
+        System.out.println("im logging with myTestGitHuber");
     }
 }
